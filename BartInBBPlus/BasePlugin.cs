68d165563a39a44d1bb364861a7f91bfa5ec056f
Elidiane <121459020+elidianesampaiotrabuco@users.noreply.github.com>
2025-11-16 15:36:52 -0300

    Add project files.

---
+++ BartInBBPlus/BasePlugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+
+using HarmonyLib;
+
+using ThinkerAPI;
+
+namespace BartInBBPlus
+{
+    [BepInPlugin("starrie.bbplus.bart", "Bart in BB+", "2.0.0.0")]
+
+    [BepInDependency("thinkerAPI")]
+
+    public class BasePlugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Harmony harmony = new Harmony("starrie.bbplus.bart");
+
+            harmony.PatchAll();
+        }
+    }
+}
